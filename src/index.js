98afa06a3054376ed65b66f698887327fddc38ee
Jose Miguel <jmn8718@gmail.com>
2016-09-26 01:23:31 +0200

    added middlewares to the server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import http from 'http';
 import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import methodOverride from 'method-override';
+import cors from 'cors';
+
 import graphqlHTTP from 'express-graphql';
 import {
   buildSchema
@@ -6,7 +14,23 @@ import {
 
 const app = express();
 
+if (process.env.ENV === 'development') {
+  app.use(logger('dev'));
+}
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(compress());
+app.use(methodOverride());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
+app.set('port', PORT);
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
@@ -30,4 +54,77 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
-app.listen(PORT, () => console.log(`Listening in port ${PORT}`));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (process.env.ENV === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message
+  });
+});
+
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(PORT);
+server.on('error', (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+server.on('listening', () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on port: ' + app.get('port'));
+});
