789ebf127c36ab9bce14f15041d26547d1ce0763
Jose Navarro <jose.navarro@famoco.com>
2016-09-26 09:38:24 +0200

    splitted server and added port

--- src/index.js
+++ src/index.js
@@ -1,90 +1,6 @@
+import app from './server';
+
 import http from 'http';
-import express from 'express';
-import logger from 'morgan';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compress from 'compression';
-import methodOverride from 'method-override';
-import cors from 'cors';
-
-import graphqlHTTP from 'express-graphql';
-import {
-  buildSchema
-} from 'graphql';
-
-const app = express();
-
-if (process.env.ENV === 'development') {
-  app.use(logger('dev'));
-}
-
-// parse body params and attache them to req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-app.use(compress());
-app.use(methodOverride());
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-
-const PORT = process.env.PORT || 3000;
-app.set('port', PORT);
-
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
-
-app.get('/', (req, res) => res.end('Hello World'));
-
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (process.env.ENV === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message
-  });
-});
-
 
 /**
  * Create HTTP server.
@@ -96,7 +12,7 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(PORT);
+server.listen(app.get('port'));
 server.on('error', (error) => {
   if (error.syscall !== 'listen') {
     throw error;
---
+++ src/server.js
@@ -0,0 +1,87 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import methodOverride from 'method-override';
+import cors from 'cors';
+
+import graphqlHTTP from 'express-graphql';
+import {
+  buildSchema
+} from 'graphql';
+
+const app = express();
+
+if (process.env.ENV === 'development') {
+  app.use(logger('dev'));
+}
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(compress());
+app.use(methodOverride());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.set('port', PORT);
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.get('/', (req, res) => res.end('Hello World'));
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (process.env.ENV === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message
+  });
+});
+
+export default app;
